2bb04cd192080e87a234498737d14d18bd2327b3
Jan Vermeer <verm0158@hz.nl>
2022-03-10 00:00:00 +0100

    Home UI

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import whiteLogo from '../images/funfit_logo_white.png'
+import colorLogo from '../images/color-logo-funfit.png'
+
+const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+
+    return (
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
+            </div>
+
+            {!authToken && !minimal && <button className="nav-button" onClick={handleClick} disabled={showModal}>Log in</button>}
+        </nav>
+    )
+}
+export default Nav
