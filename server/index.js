6f3e3500736bbb19ab83d5813bd6d6744f37ddf1
Jan Vermeer <verm0158@hz.nl>
2022-03-11 16:55:31 +0100

    Logging in

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const PORT = 8000
 const express = require('express')
-const { MongoClient } = require('mongodb')
-const { v1: uuidv4} = require('uuid')
+const {MongoClient} = require('mongodb')
+const {v1: uuidv4} = require('uuid')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
-    const { email, password } = req.body
+    const {email, password} = req.body
 
     const generateUserId = uuidv4()
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -27,7 +27,7 @@ app.post('/signup', async (req, res) => {
         const database = client.db('app-data')
         const users = database.collection('users')
 
-        const existingUser = await users.findOne({ email })
+        const existingUser = await users.findOne({email})
 
         if (existingUser) {
             return res.status((409).send('User already exists. Please login'))
@@ -47,7 +47,32 @@ app.post('/signup', async (req, res) => {
             expiresIn: 60 * 24,
         })
 
-        res.status(201).json({ token, userId: generateUserId, email: sanitizedEmail })
+        res.status(201).json({token, userId: generateUserId, email: sanitizedEmail})
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {email, password} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({token, userId: user.user_id, email})
+        }
+        res.status(400).send('Invalid Credentials')
     } catch (err) {
         console.log(err)
     }
